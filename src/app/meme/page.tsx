594616760f3210985acab102292d4d38426dc0ae
Vikesh16 <peda16032001@gmail.com>
2024-01-31 21:14:40 +0530

    added photoswipe gallery

--- src/app/meme/page.tsx
+++ src/app/meme/page.tsx
@@ -13,16 +13,22 @@
 //   return (
 //     <div>page</div>
 //   )
-// }
-import { useEffect } from 'react';
+// } 
 
-const YourComponent: React.FC = () => {
+import { useEffect ,useState} from 'react';
+import { Gallery, Item } from 'react-photoswipe-gallery'
+import 'photoswipe/dist/photoswipe.css'
+
+const MemeComponent: React.FC = () => {
+  const [data,setData]=useState<any>(null)
   useEffect(() => {
     const fetchData = async () => {
       try {
         let response = await fetch("https://www.reddit.com/r/memes.json?limit=1000");
-        let data = await response.json();
-        console.log(data);
+         response = await response.json();
+         setData(response);
+        
+        console.log(response)
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -31,12 +37,35 @@ const YourComponent: React.FC = () => {
     fetchData();
   }, []);
 
-  // Your component logic...
+ 
 
   return (
-    // Your component JSX...
-    <div>Your component content</div>
+    
+    <div>
+      {data && (
+        <div className="grid grid-cols-3 grid-rows-2 gap-12 p-5 mx-auto items-center">
+          <Gallery>
+         {data.data.children.map((e:any,index:number)=>(
+         <Item
+         key={index}
+         original={e.data.url}
+         thumbnail={e.data.url}
+         width="400"
+         height="400"
+       >
+            {({ ref, open }) => (
+        <img ref={ref} onClick={open} src={e.data.url} alt={e.data.thumbnail}  className="h-48 w-48" />
+      )}
+         
+          </Item>
+         ))}
+        </Gallery>
+         
+        </div>
+      )}
+    </div>
   );
 };
 
-export default YourComponent;
+export default MemeComponent;
+
